5f9107bc7f791b0061f359c2c71a4514a50d6cef
VilefilipeDCOMP <filipe.donascimento@dcomp.ufs.br>
2025-03-29 15:18:26 -0300

    Organizando o arquivo
    - Mudei as posições das funções e separei para deixar mais fácil de entender
    
    - Mudei a função random para aleatorio, por que no linux isso dá conflito com random do stdlib
    
    - Mudei a posição do srand para melhor aleatoridade nas respostas

--- main.c
+++ main.c
@@ -18,63 +18,29 @@ int power = 50;
 int endI;
 int endJ;
 
-long int randomizer;
+long int aleatorioizer;
 int acc = 0;
 
-//EM DESENVOLVIMENTO
-//Verifica se um determinado espaço é permitido, se sim retorna 1(true), caso contrário retorna 0(false)	
-int blockedArea (char elemento, int x, int y, int i, int j){ 
-    
-    if(elemento == '#' || elemento == '*' || elemento == '!' || i > x-1 || elemento == '@' ||i < 0 || j > y-1 || j < 0){
-        return 0;
-    }
-    else {
-        return 1;
-    }
-}
 
-void posicao(char objeto, int x, int y, char matriz[x][y]){
-	int i;
-    int j;
-    if (objeto == '@'){
-        for(i=0;i<x;i++){
-		    for(j=0;j<y;j++){
-			    if(matriz[i][j] == '@'){
-				    player.PosI = i;
-				    player.PosJ = j;
-				    return;
-			    }
-		    }
-	    }
-    }
-    else if (objeto == '$'){
-            for(i=0;i<x;i++){
-		        for(j=0;j<y;j++){
-			        if(matriz[i][j] == '$'){
-			            endI = i;
-			            endJ = j;
-				        return;
-			        }
-		        }
-	        }
-         }
-         else printf("Função definida somente para os caracteres '@' e '$' ");
-         return;
-}
-
-int random();
-int salvaArquivo();
-int verificandoArea();
-void batalha();
-void rastroPlayer();
 void printlab();
+int salvaArquivo();
+void posicao(char objeto, int x, int y, char matriz[x][y]);
+int blockedArea(char elemento, int x, int y, int i, int j);
+int verificandoArea();
+int aleatorio();
+void rastroPlayer();
+void batalha();
 void mov_aleatoria();
 
 int main(int argc, char **argv){
     char newFile[100], initFile[100], dimensao[30];
     int modo, x, y, win;
     char resp;
-    randomizer = time(NULL);
+    
+    // Cria a semente para o número aleatório 
+    aleatorioizer = time(NULL);
+    srand(aleatorioizer);
+
 	// Armazena o arquivo
     for (int i=0; i<argc; i++)
     	strcpy(initFile,argv[i]);
@@ -140,6 +106,19 @@ int main(int argc, char **argv){
     return 0;
 }
 
+// Cuida da leitura e salvamento do labirinto ------------------------------------------
+
+void printlab(int x, int y, char labirinto[x][y]){
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            printf("%c ", labirinto[i][j]);
+        }
+        if (i < y){
+            printf("\n");
+        }
+    } 
+}
+
 int salvaArquivo(char nome[100], int x, int y, char matriz[x][y]){
 	int i,j;
 	printf("Digite um nome para o arquivo (Sem o '.txt'): ");
@@ -162,6 +141,50 @@ int salvaArquivo(char nome[100], int x, int y, char matriz[x][y]){
     return 0;
 }
 
+//------------------------------------------------------------------------------------
+// Base para a movimentação ----------------------------------------------------------
+
+void posicao(char objeto, int x, int y, char matriz[x][y]){
+	int i;
+    int j;
+    if (objeto == '@'){
+        for(i=0;i<x;i++){
+		    for(j=0;j<y;j++){
+			    if(matriz[i][j] == '@'){
+				    player.PosI = i;
+				    player.PosJ = j;
+				    return;
+			    }
+		    }
+	    }
+    }
+    else if (objeto == '$'){
+            for(i=0;i<x;i++){
+		        for(j=0;j<y;j++){
+			        if(matriz[i][j] == '$'){
+			            endI = i;
+			            endJ = j;
+				        return;
+			        }
+		        }
+	        }
+         }
+         else printf("Função definida somente para os caracteres '@' e '$' ");
+         return;
+}
+
+//EM DESENVOLVIMENTO
+//Verifica se um determinado espaço é permitido, se sim retorna 1(true), caso contrário retorna 0(false)	
+int blockedArea (char elemento, int x, int y, int i, int j){ 
+    
+    if(elemento == '#' || elemento == '*' || elemento == '!' || i > x-1 || elemento == '@' ||i < 0 || j > y-1 || j < 0){
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
 int verificandoArea(int x, int y, char matriz[x][y]){
 	char N = matriz[player.PosI-1][player.PosJ], S = matriz[player.PosI+1][player.PosJ], O = matriz[player.PosI][player.PosJ-1], L = matriz[player.PosI][player.PosJ+1];
     
@@ -174,6 +197,90 @@ int verificandoArea(int x, int y, char matriz[x][y]){
     printf("N %d S %d L %d O %d\n", player.N,player.S,player.L,player.O);
 }
 
+int aleatorio(int x){
+    return rand() % x;
+}
+
+
+// int mov(int x, int y, char matriz[x][y]) {
+    //     printf("%c")
+    //     return 0;
+    // }
+
+
+
+void rastroPlayer(int posI,int posJ,int x,int y,char matriz[x][y]){
+
+    if (matriz[posI][posJ] != '@'){
+        if (matriz[posI][posJ] == '.'){
+            matriz[posI][posJ] = '*';
+        }
+        else if (matriz[posI][posJ] == '%'){
+                 batalha(posI, posJ, x, y, matriz);
+             }
+    if (matriz[posI][posJ] == '$'){
+        matriz[posI][posJ] = 'V';
+    }
+    }
+}
+
+void batalha(int i, int j, int x, int y,char matriz[x][y]){
+    int f = aleatorio(100);
+    switch (power){
+        case 50:
+            if (f < 50){
+                power += 10;
+                matriz[i][j] = '!';
+            }
+            else 
+                matriz[i][j] = '+';
+            break;
+        
+        case 60:
+            if (f < 60){
+                power += 10;
+                matriz[i][j] = '!';
+            }
+            else 
+                matriz[i][j] = '+';
+            break;
+
+        case 70:
+            if (f < 70){
+                power += 10;
+                matriz[i][j] = '!';
+            }
+            else 
+                matriz[i][j] = '+';
+            break;
+
+        case 80:
+            if (f < 80){
+                power += 10;
+                matriz[i][j] = '!';
+            }
+            else 
+                matriz[i][j] = '+';
+            break;
+
+        case 90:
+            if (f < 90){
+                power += 10;
+                matriz[i][j] = '!';
+            }
+            else 
+                matriz[i][j] = '+';
+            break;
+
+        case 100:
+                matriz[i][j] = '!';
+            break;
+    }
+}
+
+//------------------------------------------------------------------------------------
+// Algoritmo de modificação ----------------------------------------------------------
+
 void mov_aleatoria(int x, int y, char matriz[x][y]){ // retorna a próx posição
 	
     char N = matriz[player.PosI-1][player.PosJ], S = matriz[player.PosI+1][player.PosJ], O = matriz[player.PosI][player.PosJ-1], L = matriz[player.PosI][player.PosJ+1];
@@ -181,7 +288,7 @@ void mov_aleatoria(int x, int y, char matriz[x][y]){ // retorna a próx posiçã
     if (acc == 0){
         verificandoArea(x, y, matriz);
     }
-    int r = random(4);
+    int r = aleatorio(4);
     int psI = player.PosI;
     int psJ = player.PosJ;
     
@@ -234,100 +341,5 @@ void mov_aleatoria(int x, int y, char matriz[x][y]){ // retorna a próx posiçã
                                 acc = -1;
                              }
                              else mov_aleatoria(x, y, matriz); 
-    }
-}
-
-int random(int x){
-    randomizer ++;
-    srand(randomizer);
-
-    int r = rand() % x;
-    return r;
-}
-
-
-// int mov(int x, int y, char matriz[x][y]) {
-    //     printf("%c")
-    //     return 0;
-    // }
-
-
-void printlab(int x, int y, char labirinto[x][y]){
-    for (int i = 0; i < x; i++) {
-        for (int j = 0; j < y; j++) {
-            printf("%c ", labirinto[i][j]);
-        }
-        if (i < y){
-            printf("\n");
-        }
-    } 
-}
-
-void rastroPlayer(int posI,int posJ,int x,int y,char matriz[x][y]){
-
-    if (matriz[posI][posJ] != '@'){
-        if (matriz[posI][posJ] == '.'){
-            matriz[posI][posJ] = '*';
-        }
-        else if (matriz[posI][posJ] == '%'){
-                 batalha(posI, posJ, x, y, matriz);
-             }
-    if (matriz[posI][posJ] == '$'){
-        matriz[posI][posJ] = 'V';
-    }
-    }
-}
-
-void batalha(int i, int j, int x, int y,char matriz[x][y]){
-    int f = random(100);
-    switch (power){
-        case 50:
-            if (f < 50){
-                power += 10;
-                matriz[i][j] = '!';
-            }
-            else 
-                matriz[i][j] = '+';
-            break;
-        
-        case 60:
-            if (f < 60){
-                power += 10;
-                matriz[i][j] = '!';
-            }
-            else 
-                matriz[i][j] = '+';
-            break;
-
-        case 70:
-            if (f < 70){
-                power += 10;
-                matriz[i][j] = '!';
-            }
-            else 
-                matriz[i][j] = '+';
-            break;
-
-        case 80:
-            if (f < 80){
-                power += 10;
-                matriz[i][j] = '!';
-            }
-            else 
-                matriz[i][j] = '+';
-            break;
-
-        case 90:
-            if (f < 90){
-                power += 10;
-                matriz[i][j] = '!';
-            }
-            else 
-                matriz[i][j] = '+';
-            break;
-
-        case 100:
-                matriz[i][j] = '!';
-            break;
     }
 }
